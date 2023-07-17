3bc5adca7be707d331b7b3b44d24b2c8fee1649c
seokus <kbs1910@gmail.com>
2023-07-18 00:10:16 +0900

    update

--- time.js
+++ time.js
@@ -6,3 +6,16 @@ const times = `${hours}:${minutes}`;
 const chat_time = document.querySelector("#time");
 
 chat_time.innerText = times;
+
+setInterval(timeFlow, 1000);
+function timeFlow() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, 0);
+  const minutes = String(now.getMinutes()).padStart(2, 0);
+  const times = `${hours}:${minutes}`;
+
+  const chat_time = document.querySelector("#time");
+
+  chat_time.innerText = times;
+}
+timeFlow();
